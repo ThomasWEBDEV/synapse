d0fb6ca7e69616062b0fd1d920c91b1e8de3faae
Feret Thomas <thomas.feret@hotmail.fr>
2026-02-27 18:16:02 +0100

    Backend: ajout validation FluxValidatorService dans DataFluxController

--- backend/src/main/java/com/synapse/controller/DataFluxController.java
+++ backend/src/main/java/com/synapse/controller/DataFluxController.java
@@ -2,6 +2,7 @@ package com.synapse.controller;
 
 import com.synapse.model.DataFlux;
 import com.synapse.service.DataFluxService;
+import com.synapse.service.FluxValidatorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -11,9 +12,11 @@ import java.util.List;
 public class DataFluxController {
 
     private final DataFluxService service;
+    private final FluxValidatorService validator;
 
-    public DataFluxController(DataFluxService service) {
+    public DataFluxController(DataFluxService service, FluxValidatorService validator) {
         this.service = service;
+        this.validator = validator;
     }
 
     @GetMapping
@@ -28,6 +31,7 @@ public class DataFluxController {
 
     @PostMapping
     public ResponseEntity<DataFlux> create(@RequestBody DataFlux flux) {
+        validator.validate(flux);
         return ResponseEntity.ok(service.create(flux));
     }
 
